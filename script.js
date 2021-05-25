f67e7908c4e3a1125f616ace88407090cf9b9148
Anjal <anjalbeck23@gmail.com>
2021-05-25 11:36:16 +0400

    made changes to the code

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
 function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-   
+
     var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear()+ parseInt(years); 
-   
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>";
+    var year = new Date().getFullYear() + parseInt(years);
+
+    if (principal <= 0) {
+        alert("Enter a positive number")
+    }
+    else if(years < 1){
+alert("Number of years can not be empty")
+    }
+else{
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "</mark>%\<br\>You will receive an amount of <mark> " + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>";
+    }
 }
 
-function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval +"%";
-}
+
+
